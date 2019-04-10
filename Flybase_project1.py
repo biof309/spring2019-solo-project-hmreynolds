e426aa3167aeaff68532cbbccc0134c1e30f17b4
Hayley Reynolds <hmryenolds20@gmail.com>
2019-04-09 20:21:50 -0400

    Flybase_edit

--- Flybase_project1.py
+++ Flybase_project1.py
@@ -5,18 +5,21 @@ with ZipFile(fifa, 'r') as zip:
     zip.printdir()
     zip.extractall()
 import pandas as pd
+import matplotlib.pyplot as plt
 fifa1=pd.read_csv(fifa)
-print(fifa1.head())
+print(fifa1.head(10))
+print(fifa1.keys())
+print(fifa1.loc)
+print(fifa1.info())
 
-list(fifa1.columns.values)
+fifa1.columns.get_loc('Nationality')
+fifa1.columns.get_loc('Age')
+Nat_Age = fifa1.iloc[[3]]
+print(Nat_Age)
 
-from sqlalchemy import create_engine
-engine = create_engine('sqlite:///fifa1.sqlite')
-connection = engine.connect()
 
-import select
-stmt = select([fifa1])
-stmt = stmt.where(fifa1.columns.Nationality == 'American')
-results = connection.execute(stmt).fetchall()
-for result in results:
-    print(results.Nationality, results.Name)
+
+y_col = fifa1[fifa1['Age'] > 45]
+fifa1.plot(x='Nationality', y=y_col)
+plt.show()
+
