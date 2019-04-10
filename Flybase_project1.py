866778711c038432c3ff109c30a40b6f853454ac
Hayley Reynolds <hmryenolds20@gmail.com>
2019-04-10 14:43:41 -0400

    Bar graph with labels!

--- Flybase_project1.py
+++ Flybase_project1.py
@@ -1,28 +1,29 @@
-
+#%% Import zipfile and extract as read-only
 from zipfile import ZipFile
 fifa = "fifa19.zip"
 with ZipFile(fifa, 'r') as zip:
     zip.printdir()
     zip.extractall()
+#%% Import pandas and matplotlib and read in zipfile as csv
 import pandas as pd
 import matplotlib.pyplot as plt
 fifa1=pd.read_csv(fifa)
+#%% Inspect data file
 print(fifa1.head(10))
 print(fifa1.keys())
 print(fifa1.info())
-
+#%% Identify columns of interest and isolate them from data set
 fifaNA = fifa1[['Nationality','Age']]
 print(fifaNA)
-
+#%% Group by Nationality to merge identical names and aggregate to mean to find\
+#%% mean age per Nationality (set Nationality to index)
 Group = fifaNA.groupby(['Nationality'], as_index=True).agg('mean')
 print(Group)
-
-Nat = fifaNA[['Nationality']]
-Nat_drop = Nat.drop(axis=0)
-print(Nat_drop)
-
-Group_graph = Group.reset_index().plot(x='Nationality', y='Age')
+#%% Plot bar graph setting x- and y- axes
+Group_graph = Group.reset_index().plot.bar(x='Nationality', y='Age')
+#%% Label axes and show plot
 plt.xlabel('Nationality')
 plt.ylabel('Mean Age')
 plt.show()
+
 
