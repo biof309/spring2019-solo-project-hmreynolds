a2aa78c4c4d8fe40bd51a96122d5b6d43f17bdee
hmreynolds <47371170+hmreynolds@users.noreply.github.com>
2019-05-03 12:25:01 -0400

    Delete flybase.py

--- flybase.py
+++
@@ -1,9 +0,0 @@
-from zipfile import ZipFile
-transgene = "construct_maps.zip"
-with ZipFile(transgene, 'r') as zip:
-    zip.printdir()
-    zip.extractall()
-import pandas as pd
-import matplotlib as plt
-flybase = pd.read_csv(transgene)
-print(flybase[0].head())
